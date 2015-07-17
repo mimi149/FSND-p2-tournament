7d1f1eb4d61a7f0348e6aa60aa56d311f49a7436
mimi149 <baotranmimi@gmail.com>
2015-07-16 18:17:27 -0700

    Update tournament.sql
    Update tournament.sql so that it can connect to the database after creating.

--- p2_tournament/tournament.sql
+++ p2_tournament/tournament.sql
@@ -1,13 +1,10 @@
 -- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
-DROP DATABASE IF EXISTS tournament;
 
+-- Create database "tournament" and connect to that database before creating tables
+\c vagrant
+DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
+\c tournament
 
 DROP TABLE IF EXISTS Matches;
 DROP TABLE IF EXISTS Players CASCADE;
